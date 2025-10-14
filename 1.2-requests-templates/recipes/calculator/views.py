b29a230b3d7acdfcb9fb637ced8704c091a7ac14
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2025-10-14 16:07:00 +0200

    recipe exercise is done

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,8 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
@@ -28,3 +30,42 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+#
+
+def omlet(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {}
+    }
+
+    for ingredients, amount in DATA['omlet'].items():
+        context['recipe'][ingredients] = amount * servings
+
+
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {}
+    }
+
+    for ingredients, amount in DATA['pasta'].items():
+        context['recipe'][ingredients] = amount * servings
+
+
+    return render(request, 'calculator/index.html', context)
+
+
+def buter(request):
+    servings = int(request.GET.get('servings', 1))
+    context = {
+        'recipe': {}
+    }
+
+    for ingredients, amount in DATA['buter'].items():
+        context['recipe'][ingredients] = amount * servings
+
+
+    return render(request, 'calculator/index.html', context)
